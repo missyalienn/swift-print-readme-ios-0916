e00f3e7882864e0b6b5176d6d2331c0e97501716
Missy Allan <missyallan@Melissas-MacBook-Air.local>
2016-09-07 16:22:04 -0400

    Swift Print & interpolation

---
+++ PrintPlayground.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var favoriteWord = "loquacious"
+var favoriteColor = "purple"
+var favoriteMusician = "Chance the Rapper"
+
+print(favoriteWord)
+
+print(favoriteColor)
+
+print(favoriteMusician)
+
+print("I'm printing a string in Swift!")
+
+print("Hi Mom! My favorite word is serendipity, my favorite color is green, and my favorite musician is Billy Joel.")
+
+let color = "blue"
+
+print ("The sky is \(color).")
+
+print ("Hi Dad! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
+
+
+let dogName = "Panda"
+
+var dogSentence = "The dog's name is \(dogName)."
+
+print (dogSentence)
